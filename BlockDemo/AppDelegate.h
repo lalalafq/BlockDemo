ade247ca28efec1b72fa48aacf9d7f4869f15c96
fuqi <wb-fq258946@alibaba-inc.com>
2017-01-11 20:16:44 +0800

    Initial Commit

---
+++ BlockDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BlockDemo
+//
+//  Created by lifeng on 17/1/11.
+//  Copyright © 2017年 fq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
